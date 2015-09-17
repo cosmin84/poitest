ba86e1e3f9ac7219f752426f279cc4d1c0403f06
Cosmin Chiru <cosmin.chiru@gmail.com>
2015-09-17 22:05:41 +0300

    Initial commit

---
+++ src/poitest/ReadExcel.java
@@ -0,0 +1,72 @@
+package poitest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.CellReference;
+import org.apache.poi.ss.usermodel.*;
+
+public class ReadExcel {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// Will contain cell name / value pair for input cells and output cells
+		Map<String, String> inputCellsMap = new HashMap<String, String>();
+		Map<String, String> outputCellsMap = new HashMap<String, String>();
+		
+		// Open the Excel file
+		FileInputStream file = new FileInputStream(new File(args[1]));
+		
+		// Get the current workbook
+		HSSFWorkbook workbook = new HSSFWorkbook(file);
+		
+		// Get the first sheet of the workbook
+		HSSFSheet sheet = workbook.getSheetAt(0);
+		
+		// Get the input cells that need to be modified and
+		// store their name and value in the inputCellsMap
+		for (String element : args[3].split(";")) {
+			inputCellsMap.put(element.split("=")[0], element.split("=")[1]);
+		}
+		
+		// Loop through the cells that need to be modified and 
+		// set the new value in the Excel document
+		Iterator<Entry<String,String>> iterator = inputCellsMap.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Map.Entry<String,String> entry = (Map.Entry<String,String>) iterator.next();
+
+			CellReference cr = new CellReference(entry.getKey());
+			int r = cr.getRow();
+			int c = cr.getCol();
+
+			Row row = sheet.getRow(r);
+			if (row == null)
+			    row = sheet.createRow(r);
+			Cell cell = row.getCell(c, Row.CREATE_NULL_AS_BLANK);				
+			cell.setCellValue(Integer.parseInt(entry.getValue()));		
+		}
+					
+		// Apply all formulas after altering cell values
+		HSSFFormulaEvaluator.evaluateAllFormulaCells(workbook);
+
+		// Get result
+		// This is currently hard coded
+		// Will have to get results from the cells entered as args
+		Cell resultCell = sheet.getRow(6).getCell(1);
+		
+		Cell someCell = sheet.getRow(3).getCell(1);
+
+		System.out.println(resultCell.getNumericCellValue());	
+						
+		workbook.close();		
+	}
+}
